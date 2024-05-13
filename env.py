18887e826af2294e25f54b174f4c126264c0b7c0
benamos216 <benamos216@gmail.com>
2024-05-13 20:56:38 +0000

    Deployment Commit

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://uf29ndcffr18ta:pa0d395dbab3ac1b59aab0b3cbeec8e6c577dc7abf44677f0a23b89f2fd588f72@c7u1tn6bvvsodf.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/ddfh6dqon3dhml"
+os.environ["SECRET_KEY"] = "iai6uh&2yv$#+=h&1yzr0)=x%pkd=cpiaqwu&s*_@l*d$gtt+5"
+os.environ["CLOUDINARY_URL"] = "cloudinary://542198291661993:F2j7DzGGfZNIvK-65xEE4CEguNQ@dw88orkqf"
